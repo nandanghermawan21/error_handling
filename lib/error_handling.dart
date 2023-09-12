388fafd60cb5482d885f61b3179fa3b0bfa3c74f
Nandang Hermawan <nadanghermawan2014@gmail.com>
2023-09-12 14:56:05 +0700

    commit

--- lib/error_handling.dart
+++ lib/error_handling.dart
@@ -1,7 +1,50 @@
 library error_handling;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:error_handling/basic_response.dart';
+import 'package:http/http.dart' as http;
+
+class ErrorHandlingUtil {
+  static handleApiError(
+    dynamic error, {
+    String? prefix = "",
+    String? onTimeOut = "",
+    String? unAuthorizedMessageError = "",
+  }) {
+    String _message = "";
+    if (error is BasicResponse) {
+      _message = error.message ?? "";
+    } else if (error is FormatException) {
+      _message = error.toString();
+    } else if (error is http.Response) {
+      switch (error.statusCode) {
+        case 401:
+          _message = unAuthorizedMessageError ?? "Unauthorized";
+          break;
+        default:
+          _message = error.body;
+      }
+    } else {
+      _message = error.toString();
+    }
+
+    _message = "$prefix $_message";
+
+    return _message.replaceAll('"', "");
+  }
+
+  static String readMessage(http.Response response) {
+    try {
+      return json.decode(response.body)["Message"].toString() == ""
+          ? defaultMessage(response)
+          : json.decode(response.body)["Message"].toString();
+    } catch (e) {
+      return defaultMessage(response);
+    }
+  }
+
+  static String defaultMessage(http.Response response) {
+    return "${response.body.isNotEmpty ? response.body : response.statusCode}";
+  }
 }
